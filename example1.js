a0778ccf12cecaeb1ee4488e67566c8aa9e78240
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-05-08 23:47:34 +0900

    05_08_study

---
+++ example1.js
@@ -0,0 +1,12 @@
+const nicoInfo = {
+  name: 'Nico',
+  age: 33,
+  gender: 'Male',
+  isHandsome: true,
+};
+
+// Object에 key값이 없으므로.. undefiend가 출력
+console.log(nicoInfo.favFood);
+
+// 각 funcfion의 key값이 출력된다.
+console.log(nicoInfo, console);
